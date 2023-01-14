dbfc1dbc509bd3bc0c6205fca48ddfe14ce329eb
s452645 <bargro6@st.amu.edu.pl>
2023-01-14 18:48:54 +0100

    Configure frontend - Tauri & Angular

---
+++ frontend/race-director-frontend/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { PrimeNGConfig } from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { invoke } from '@tauri-apps/api'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'race-director-frontend';
+
+  constructor(private primengConfig: PrimeNGConfig, private translateService: TranslateService) { }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    this.translateService.setDefaultLang('en');
+
+    // invoking rust function ('command')
+    invoke('greet', { name: 'Bartek' })
+      .then((response) => console.log(response));
+  }
+
+  translate(lang: string) {
+    this.translateService.use(lang);
+    this.translateService.get('primeng').subscribe(res => this.primengConfig.setTranslation(res));
+  }
+}
---
+++ frontend/race-director-frontend/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { ButtonModule } from 'primeng/button';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
