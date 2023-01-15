aa6a5d29388cb42547a9d4a20c85b2689b3e8293
s452645 <bargro6@st.amu.edu.pl>
2023-01-15 13:46:51 +0100

    Configure splashscreen

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -3,6 +3,18 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
+// use if splashscreen need to wait for a call from frontend
+// #[tauri::command]
+// async fn close_splashscreen(window: tauri::Window) {
+//   if let Some(splashscreen) = window.get_window("splashscreen") {
+//     splashscreen.close().unwrap();
+//   }
+//   // Show main window
+//   window.get_window("main").unwrap().show().unwrap();
+// }
+
 #[tauri::command]
 fn greet(name: &str) -> String {
    format!("Hello, {}!", name)
@@ -10,7 +22,23 @@ fn greet(name: &str) -> String {
 
 fn main() {
   tauri::Builder::default()
+  .setup(|app| {
+    let splashscreen_window = app.get_window("splashscreen").unwrap();
+    let main_window = app.get_window("main").unwrap();
+    // we perform the initialization code on a new task so the app doesn't freeze
+    tauri::async_runtime::spawn(async move {
+      // initialize your app here instead of sleeping :)
+      println!("Initializing...");
+      std::thread::sleep(std::time::Duration::from_secs(10));
+      println!("Done initializing.");
+
+      // After it's done, close the splashscreen and display the main window
+      splashscreen_window.close().unwrap();
+      main_window.show().unwrap();
+    });
+    Ok(())
+  })
   .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  .run(tauri::generate_context!())
+  .expect("error while running tauri application");
 }
